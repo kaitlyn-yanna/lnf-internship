e2723d2fa8af7c40ee39ee5289ba99d1fe434961
kaitlyn yanna <138136153+kaitlyn-yanna@users.noreply.github.com>
2023-07-12 12:14:52 +0200

    Add files via upload

--- chopsignal.m
+++ chopsignal.m
@@ -4,6 +4,10 @@ function [M, centers] = chopsignal(x, window, overlap)
 %   signal in the columns so that, for instance, S = skewness(M) is a 1D
 %   signal with the skewness of each of the signal windows.
 %
+%   upon further investigation, it was found that 'window' is not actually
+%   the number of windows, but the number of points in such a window. thus
+%   when overlap = 0, actual_window = length(x)/window
+%
 %    M = chopsignal(x, window, overlap)
 %
 %   Examples:
@@ -13,13 +17,14 @@ function [M, centers] = chopsignal(x, window, overlap)
 
 if overlap > window; overlap = window - 1; end
 
-starts = 1:(window - overlap):(length(x) - window + 1) ;
+starts = 1:(window - overlap):(length(x) - window + 1); 
+
 M = zeros(window, length(starts));
+
 
 for i=1:length(starts)
     M(:,i) = x(starts(i):starts(i) + window -1);
 end
 
 centers = starts + round(.5*window);
-
 end
